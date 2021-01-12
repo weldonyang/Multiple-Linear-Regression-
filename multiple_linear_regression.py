72b885371ac52b6f63f55eaa2d60e1a20b15dc09
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-12 14:32:13 -0800

    First commit

---
+++ multiple_linear_regression.py
@@ -0,0 +1,16 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+# Encoding categorical data
+
+# Splitting the dataset into the Training set and Test set
+
+# Training the Multiple Linear Regression model on the Training set
+
+# Predicting the Test set results
