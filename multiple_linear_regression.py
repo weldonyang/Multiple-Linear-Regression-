2d38c1a9474b75195946a497b13b38323d151eeb
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-12 17:30:40 -0800

    Update multiple_linear_regression.py
    - Trained MLR model
    - Compared predicted test values against actual test values

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -9,7 +9,6 @@ import pandas as pd
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-print(X)
 
 # Encoding categorical data
 from sklearn.compose import ColumnTransformer
@@ -22,5 +21,22 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Training the Multiple Linear Regression model on the Training set
+# sklearn will avoid dummy variable trap
+# sklearn will pick the model with the lowest p-value 
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# NOTE: Due to there being more than one independent variable, we cannot simply plot it on a 2D graph (in this case, we would need a 5D graph)
+#       so we will display 2 vectors: 1. real profits of test set and 
+#                                     2. predicted profits of test set 
 
 # Predicting the Test set results
+# np.set_printoptions(precision=2) displays any numerical values with 2 decimals 
+# np.concatenate() expects 2 args: 1. tuple of arrays 
+#                                  2. axis=0 or 1 (0 means vertical concatenation, 1 means horizontal )
+# .reshape() is a function of regressor that takes 2 args: 1. # of rows
+#                                                          2. # of columns and reshapes the vector
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)                                                      
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))                           
