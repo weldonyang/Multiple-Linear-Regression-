0e6127cd31a65189f2d80683eaf3832123f9050a
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-12 17:04:42 -0800

    Update multiple_linear_regression.py
    - Imported data set
    - Encoded catagorical data into dummy variables
    - Split datset into training and test sets

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,10 +6,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
 
 # Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 
 # Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Training the Multiple Linear Regression model on the Training set
 
